82561bbe7ea23c6386bbcea965b312e5ce56b000
Vladimir_M <vovqaz@gmail.com>
2022-12-30 17:23:54 +0300

    change program del Line

--- Example 1/Program.cs
+++ Example 1/Program.cs
@@ -1,5 +1,5 @@
-﻿Console.WriteLine("Введите имя");
+﻿Console.Write("Введите имя");
 string username = Console.ReadLine();
-Console.WriteLine("Привет,");
-Console.WriteLine(username);
+Console.Write("Привет,");
+Console.Write(username);
 
